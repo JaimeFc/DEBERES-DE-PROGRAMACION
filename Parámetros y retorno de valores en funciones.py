bc2dbe64dc9cdc72bc9591d441b3b34a2bc13f5d
ADMINISTRADOR 1 <jf.castillog@uea.edu.ec>
2024-03-16 15:34:07 -0500

    calcular_descuento

---
+++ Parámetros y retorno de valores en funciones.py
@@ -0,0 +1,10 @@
+# #Hallar el valor a pagar de una compra sabiendo que hay descuento del 10%
+# para todos los clientes y saber cual es el ahorro del descuento del 10%"
+def calcular_descuento(total, descuento=10):
+    descuento= (total * descuento)/100
+    return descuento
+monto_total= int(input("ingrese el valor:"))
+descuento=calcular_descuento(monto_total)
+monto_total=monto_total-descuento
+print(f"monto total a pagar:{monto_total}")
+print(f"descuento del 10%:{descuento}")
